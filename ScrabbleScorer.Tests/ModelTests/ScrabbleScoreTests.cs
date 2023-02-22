5e3fe14a463cd0838e760e8624a97baf2e2b4275
Justin Lee <lee.justin001126@gmail.com>
2023-02-22 14:25:58 -0800

    created a good fail test

---
+++ ScrabbleScorer.Tests/ModelTests/ScrabbleScoreTests.cs
@@ -0,0 +1,17 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ScrabbleScore.Models;
+
+
+namespace ScrabbleScore.Tests
+{
+  [TestClass]
+  public class ScrabbleCalculate
+  {
+    [TestMethod]
+    public void ScrabbleCalculate_HasArrayWithValues_DictionaryWithPointers()
+    {
+      ScrabbleCalculate newWord = new Word();
+    }
+
+  }
+}
